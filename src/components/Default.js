d678a96e3ff7d6b494ee04f95242a5b61ea57039
Jacobo Martinez <jacobojavier98@gmail.com>
2019-08-27 21:23:38 -0400

    Setup 404 component

--- src/components/Default.js
+++ src/components/Default.js
@@ -1,9 +1,20 @@
 import React from 'react';
 
-const Default = () => {
+const Default = props => {
+  console.log(props);
   return (
-    <section>
-      <h3>Page Not Found</h3>
+    <section className="container">
+      <div className="row">
+        <div className="col-10 mx-auto text-uppercase text-title text-center pt-5">
+          <h1 className="display-3 font-weight-bold">404</h1>
+          <h2 className="font-weight-bold">page not found</h2>
+          <h3>
+            The requested URL{' '}
+            <span className="text-danger">{props.location.pathname}</span> was
+            not found
+          </h3>
+        </div>
+      </div>
     </section>
   );
 };
